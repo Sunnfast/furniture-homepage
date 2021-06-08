ad0db2c259826984a56d0ea574f37c49d5c3256f
Simone Christen <simone.k.christen@gmail.com>
2021-06-07 20:51:23 -0700

    set up basic grid. still needs adjusting, specifically on the javascript element.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // dynamic header, flavor text, and shop now
 
-let dyanmicDisplay =  "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+let dynamicDisplay =  "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
 let dynamicHeader = "Discover innovative \n ways to decorate"
 
 dynamicHeaderText();
@@ -9,6 +9,7 @@ dynamicText();
 // dynamic header
 function dynamicHeaderText() {
 const dynamicHeaderStyle = document.getElementById("dynamic-header").style;
+document.getElementById("dynamic-header").classList = "item2";
 document.getElementById("dynamic-header").innerText = dynamicHeader;
 dynamicHeaderStyle.fontWeight = "600";
 // dynamicHeaderStyle.border = "solid 2px black";
@@ -23,7 +24,7 @@ dynamicHeaderStyle.left = "925px";
 // these are flex bc the parent node is flex!! [delete me]
 function dynamicText() {
     const dynamicStyle = document.getElementById("dynamic-text").style;
-    document.getElementById("dynamic-text").innerText = dyanmicDisplay;
+    document.getElementById("dynamic-text").innerText = dynamicDisplay;
     dynamicStyle.color = "var(--darkGray)";
     dynamicStyle.lineHeight = '1.5em';
     dynamicStyle.fontFamily = 'Spartan';
